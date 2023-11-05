c3313bc663cdcc3f1872a9b0aae838a548901163
Zhou Manze <manzezz1996@gmail.com>
2023-11-05 12:40:53 -0500

    connected mysql with node server

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const { faker } = require('@faker-js/faker');
+require('dotenv').config();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : process.env.DB_PASSWORD,
+    database : 'join_us'
+});
+
+const q = 'SELECT CURTIME() as time, CURDATE() as date, NOW() as now';
+
+connection.query(q, function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+});
+
+connection.end();
+
+// console.log(faker.internet.email());
